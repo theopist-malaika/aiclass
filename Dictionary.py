c7aeeae71df083de55e664e33bbb0fe1f7bcff8d
theopist-malaika <theopistmalaika@gmail.com>
2025-08-14 10:21:50 +0200

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,5 @@
+contact_book={"Emma":"0993046123","Eric":"0999636478","Theo":"0887585317","Ma Joshua":"0995624548"}
+
+userlnput=input("Enter the  Name:")
+
+print(contact_book.get(userlnput,"Not found!"))
